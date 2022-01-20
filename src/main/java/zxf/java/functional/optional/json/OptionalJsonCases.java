a22fd1fb2811c78663c2b1420b601db1489ec949
davis <zanxiaofeng@163.com>
2022-01-20 14:39:48 +0800

    update optional

---
+++ src/main/java/zxf/java/functional/optional/json/OptionalJsonCases.java
@@ -0,0 +1,22 @@
+package zxf.java.functional.optional.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class OptionalJsonCases {
+    public static void main(String[] args) throws IOException {
+        Book book = new Book();
+        book.setTitle("Test book");
+        book.setSubTitle(Optional.of("this is sub title"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
+        String json = mapper.writeValueAsString(book);
+        System.out.println(json);
+        Book newBook = mapper.readValue(json, Book.class);
+
+    }
+}
