fd40fbac10fd8da9d23630f7c9bb6b9f7e5b3dc8
davis <zanxiaofeng@163.com>
2022-02-20 11:21:18 +0800

    update

--- src/main/java/zxf/java/functional/function/FunctionalUsage.java
+++ src/main/java/zxf/java/functional/function/FunctionalUsage.java
@@ -10,9 +10,10 @@ import static java.nio.file.StandardOpenOption.CREATE;
 
 public class FunctionalUsage {
     public static void main(String[] args) {
-        //一个函数值是否可以赋值给一个函数类型，首先要看函数值的参数类型、参数顺序、返回值类型是否与函数类型声明的形式兼容一致
-        //其次要看函数值声明的Exceptions(Throws子句)能否被函数类型声明的Exceptions(Throws子句)覆盖包含
-        //立即函数-Lambda没有Throws子句，因此不用考虑Exception的兼容性
+        //一个函数值是否可以赋值给一个函数类型，首先要看函数值的参数类型、参数顺序、返回值类型是否与函数类型声明的形式兼容一致，
+        //其次要看函数值声明的Exceptions(Throws子句)能否被函数类型声明的Exceptions(Throws子句)覆盖包含，
+        //立即函数-Lambda虽然没有显式的Throws子句，但编译器可以依据其代码中调用的函数的签名汇总出其可能Throw的Ｅxceptions，
+        //因此也要符合这个约束条件
         //BiConsumer<Path, String> fileLogger = FunctionalUsage::log;
     }
 
