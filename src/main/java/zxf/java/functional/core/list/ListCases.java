b03c295d41c17739b8993bea81686fd8bf2fb3ba
davis <zanxiaofeng@163.com>
2022-02-22 20:31:24 +0800

    update

--- src/main/java/zxf/java/functional/core/list/ListCases.java
+++ src/main/java/zxf/java/functional/core/list/ListCases.java
@@ -7,21 +7,51 @@ import java.util.function.Function;
 
 public class ListCases {
     public static void main(String[] args) throws Exception {
+        use_case1();
+        use_case2();
         use_case3();
+    }
+
+    //Functor
+    public static void use_case1() {
+        System.out.println("use_case1");
+        List<Integer> numbers = new List<>(23, 45, 67);
+        List<String> strings = numbers.map(ListCases::add10);
+    }
+
+    //Monad
+    public static void use_case2() {
+        System.out.println("use_case2");
+        List<Integer> numbers = new List<>(23, 45, 67);
+        List<String> strings = numbers.flatMap(ListCases::add100);
     }
 
     //Applicative--左结合，第一个参数用函子map，后面的参数用应用子apply
     public static void use_case3() throws Exception {
-        List<Integer> list1 = new List<>(23, 45, 67);
-        List<Integer> list2 = new List<>(10, 100, 200);
-
+        System.out.println("use_case3");
         Function<Integer, CheckedFunction<Integer, String>> curriedTriFunction = Currying.curryingFunction(ListCases::mulAndToString);
-        List<CheckedFunction<Integer, String>> result1 = list1.map(curriedTriFunction);
-        List<String> result = list2.applyChecked(result1);
+
+        List<Integer> numbersLeft = new List<>(23, 45, 67);
+        List<CheckedFunction<Integer, String>> middleResult = numbersLeft.map(curriedTriFunction);
+        List<Integer> numbersRight = new List<>(10, 100, 200);
+        List<String> finalResult = numbersRight.applyChecked(middleResult);
     }
 
-    public static String mulAndToString(Integer a, Integer b) {
-        System.out.println(String.format("%d * %d = %d", a, b, a * b));
-        return String.valueOf(a * b);
+    private static String add10(Integer value) {
+        String result = String.valueOf(value + 10);
+        System.out.println(String.format("%d + 10 = %s", value, result));
+        return result;
+    }
+
+    private static List<String> add100(Integer value) {
+        String result = String.valueOf(value + 100);
+        System.out.println(String.format("%d, %s", value, result));
+        return new List<>(value.toString(), result);
+    }
+
+    private static String mulAndToString(Integer left, Integer right) {
+        String result = String.valueOf(left * right);
+        System.out.println(String.format("%d * %d = %s", left, right, result));
+        return result;
     }
 }
--- src/main/java/zxf/java/functional/core/optional/OptionalCases.java
+++ src/main/java/zxf/java/functional/core/optional/OptionalCases.java
@@ -16,6 +16,7 @@ public class OptionalCases {
 
     //Functor
     public static void use_case1() {
+        System.out.println("use_case1");
         Optional<Integer> optional999 = new Optional<>(999);
         Optional<String> optional999Add10 = optional999.map(OptionalCases::add10);
         System.out.println(optional999Add10.get());
@@ -27,6 +28,7 @@ public class OptionalCases {
 
     //Monad
     public static void use_case2() {
+        System.out.println("use_case2");
         Optional<Integer> optional999 = new Optional<>(999);
         Optional<String> optional999Add100 = optional999.flatMap(OptionalCases::add100);
         System.out.println(optional999Add100.get());
@@ -38,26 +40,28 @@ public class OptionalCases {
 
     //Applicative--左结合，第一个参数用函子map，后面的参数用应用子apply
     public static void use_case3() throws Exception {
+        System.out.println("use_case3");
+        Function<Integer, CheckedFunction<Integer, String>> curriedTriFunction = Currying.curryingFunction(OptionalCases::mulAndToString);
+
         Optional<Integer> optional10 = new Optional<>(10);
         Optional<Integer> optional999 = new Optional<>(999);
-
-        Function<Integer, CheckedFunction<Integer, String>> curriedTriFunction = Currying.curryingFunction(OptionalCases::mulAndToString);
         Optional<String> result = optional999.applyChecked(optional10.map(curriedTriFunction));
         System.out.println(result.get());
     }
 
     //right
     public static void use_case4() throws Exception {
+        System.out.println("use_case4");
+        OptionalCurrying.BiCurryingFunction<Integer, Integer, String> curriedTriFunction = OptionalCurrying.curryingFunction(OptionalCases::mulAndToString);
+
         Optional<Integer> optional10 = new Optional<>(10);
         Optional<Integer> optional999 = new Optional<>(999);
-
-        OptionalCurrying.BiCurryingFunction<Integer, Integer, String> curriedTriFunction = OptionalCurrying.curryingFunction(OptionalCases::mulAndToString);
         Optional<String> result = curriedTriFunction.apply(optional10).apply(optional999);
         System.out.println(result.get());
     }
 
     public static String add10(Integer value) {
-        return value.toString();
+        return String.valueOf(value + 10);
     }
 
     public static Optional<String> add100(Integer value) {
@@ -67,8 +71,9 @@ public class OptionalCases {
         return new Optional<>(String.valueOf(value + 100));
     }
 
-    public static String mulAndToString(Integer a, Integer b) {
-        System.out.println(String.format("%d * %d = %d", a, b, a * b));
-        return String.valueOf(a * b);
+    public static String mulAndToString(Integer left, Integer right) {
+        String result = String.valueOf(left * right);
+        System.out.println(String.format("%d * %d = %s", left, right, result));
+        return result;
     }
 }
